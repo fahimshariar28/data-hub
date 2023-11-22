8a5bfb04d88221e9c821058f7f195d69b0873f7a
Fahim Shariar <fahimshariar28@gmail.com>
2023-11-22 23:53:24 +0600

    Interface Decleared for users

---
+++ src/app/modules/user/user.interface.ts
@@ -0,0 +1,33 @@
+// Type for User Full Name
+export type TFullName = {
+  firstName: string;
+  lastName: string;
+};
+
+// Type for User Address
+export type TAddress = {
+  street: string;
+  city: string;
+  country: string;
+};
+
+// Type for User Order
+export type TOrder = {
+  productName: string;
+  price: number;
+  quantity: number;
+};
+
+// Type for User
+export type TUser = {
+  userId: number;
+  userName: string;
+  password: string;
+  fullName: TFullName;
+  age: number;
+  email: string;
+  isActivate: boolean;
+  hobbies: string[];
+  address: TAddress;
+  orders?: TOrder[];
+};
