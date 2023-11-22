a836c498ad15fbbd720d92aea03c321ca097d3db
Fahim Shariar <fahimshariar28@gmail.com>
2023-11-23 01:45:46 +0600

    route created to get all users

--- src/app/modules/user/user.router.ts
+++ src/app/modules/user/user.router.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
-import { createUserController } from './user.controller';
+import { createUserController, getAllUsersController } from './user.controller';
 
 const router = Router();
 
 // POST endpoint for creating a new user
 router.post('/', createUserController);
+
+// GET endpoint for getting all users
+router.get('/', getAllUsersController);
 
 export const UserRoutes = router;
